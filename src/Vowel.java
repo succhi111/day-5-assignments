41fbd0e69775f4a263e3a427af2a37427a9a2104
succhi111 <suchendramishra396@gmail.com>
2022-08-18 19:56:54 +0530

    program to find vowels consonents

--- src/Vowel.java
+++ src/Vowel.java
@@ -1,2 +1,25 @@
-public class Vowel {
-}
+public class Vowel { public static void main(String[] args) {
+            char ch='m';
+            switch(ch)
+            {
+                case 'a':
+                    System.out.println("Vowel");
+                    break;
+                case 'e':
+                    System.out.println("Vowel");
+                    break;
+                case 'i':
+                    System.out.println("Vowel");
+                    break;
+                case 'o':
+                    System.out.println("Vowel");
+                    break;
+                case 'u':
+                    System.out.println("Vowel");
+                    break;
+                default:
+                    System.out.println("Consonant");
+            }
+        }
+    }
+
