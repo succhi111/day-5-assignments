db13253e505b17c4b0e48fd6f3980a236fefba05
succhi111 <suchendramishra396@gmail.com>
2022-08-22 20:55:17 +0530

    program for power of 2

--- src/PowerOf2.java
+++ src/PowerOf2.java
@@ -1,3 +1,18 @@
-public class PowerOf2 {
+import java.util.Scanner;
 
+public class PowerOf2 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter power of 2");
+        int x = sc.nextInt();
+        int n = 2;
+        int result = 1;
+        for (int i = 1; i <= x; i++) {
+            result = n * result;
+
+        }
+        System.out.println(result);
+    }
 }
